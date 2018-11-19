3eeefa9a4e4ecfdd9feb565c560c50ef7b24ec6b
taotao <liuhuiyu@qiandongdong.cn>
2018-11-19 16:25:40 +0800

    add amend.sh

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # blog
-technical &amp; personal life
+
+关注公众号三岁嘟嘟获取更多信息：
+
+![](/Users/kittaaron/git/blog/images/open_qr_code.jpg)
+
---
+++ sendmail.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#--------------------------------------------
+# 监听服务器日志, 如果出现错误，发邮件告警
+# author：菜鸟教程
+# site：www.runoob.com
+# slogan：学的不仅是技术，更是梦想！
+#--------------------------------------------
+
+cd `dirname $0`
+logfile='logs/out_sports.log'
+beforelinenum=`sed -n '$=' logs/out_sports.log`
+echo $beforelinenum
+echo "开始监听..."
+secondspan=3
+ip=`ip addr | awk '/^[0-9]+: / {}; /inet.*global/ {print gensub(/(.*)\/(.*)/, "\\1", "g", $2)}'`
+while true
+do
+  afterlinenum=`sed -n '$=' logs/out_sports.log`
+  line=$(( $afterlinenum - $beforelinenum ))
+  #echo "line:"$line
+  beforelinenum=$afterlinenum
+  content=`tail -n $line logs/out_sports.log |grep -A 20 'Exception'`
+  if [ -n "$content" ]; then
+    echo -e "服务器发生异常,准备发送告警邮件"
+    echo -e $content  |  mail  -s  "服务器$ip 发生异常，请及时关注" -r liuhuiyu@qiandongdong.cn liuhuiyu5201413@163.com,liuhuiyu@qiandongdong.cn,husaiping@qiandongdong.cn,yanglinfeng@qiandongdong.cn,yangjingjing@qiandongdong.cn,wuyeran@qiandongdong.cn	
+    echo "发送邮件完成..."
+  fi
+  sleep $secondspan
+done
+echo "程序退出."
+exit
